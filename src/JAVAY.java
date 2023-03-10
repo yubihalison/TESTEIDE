c4d052cdeee32289cc5da922cd99aa58c8974d5d
yubi <yubihalison16@gmail.com>
2023-03-09 22:35:24 -0300

    first comit

---
+++ src/JAVAY.java
@@ -0,0 +1,22 @@
+import gato.com.br.gato;
+
+public class JAVAY {
+
+	public static void main(String[] args) {
+	
+		gato gato = new gato();
+		livros livros = new livros();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		  int b = 3;
+	System.out.println("Hellow world! " + (a+b));*/	
+	}
+	
+}
+
+class livros {
+	private String nome;
+	private String npag;
+}
---
+++ src/gato/com/br/gato.java
@@ -0,0 +1,50 @@
+package gato.com.br;
+
+public class gato {
+	
+private String nome;
+private String cor;
+private String idade;
+
+public gato() {}
+
+public gato(String nome, String cor, String idade) {
+	this.nome = nome;
+	this.cor = cor;
+	this.idade = idade;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getCor() {
+	return cor;
+}
+
+public void setCor(String cor) {
+	this.cor = cor;
+}
+
+public String getIdade() {
+	return idade;
+}
+
+public void setIdade(String idade) {
+	this.idade = idade;
+
+}}
+
+/*public gato(String nome, String cor, String idade) {
+	this.nome = nome;
+	this.cor = cor;
+	this.idade = idade;
+   }
+}*/
+
+
+
